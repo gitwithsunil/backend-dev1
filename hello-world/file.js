c91c58ca2100f8f8d9899bc9e0c9b3754e11554f
gitwithsunil <reddysu81@gmail.com>
2026-06-01 12:49:59 +0530

    read

--- hello-world/file.js
+++ hello-world/file.js
@@ -1,8 +1,9 @@
-const fs = require('fs');
+const fs = require('fs')
 // synchronous
 // fs.writeFileSync('./test.txt', 'Hello  World!')
  
 // asynchronous
 // fs.writeFile('./test.txt', "hello sunil" , (err) => {})
 
-    
+const result = fs.readFileSync('./contact.txt', 'utf-8');
+console.log(result);
