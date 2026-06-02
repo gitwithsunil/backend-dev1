0943d2d5c7a4421996f094dc52680e562a06b76b
gitwithsunil <reddysu81@gmail.com>
2026-06-02 12:05:43 +0530

    server tets

--- HTTP-Server/index.js
+++ HTTP-Server/index.js
@@ -1,8 +1,11 @@
 const http = require('http');
 
 const myServer = http.createServer((req, res) => {
-    console.log(req)
-    res.end("Hello from my server");
+    // console.log(req.headers) // this will log the headers of the request
+    // console.log(req.url) // this will log the url of the request
+    //console.log("new request received") // this will log a message every time a new request is received
+    console.log("new request received");
+    res.end("Hello from my server again!");
 })
 
-myServer.listen(8000, () => console.log("Server is running on port 8000..."))
+myServer.listen(8000, () => console.log("Server started"))
